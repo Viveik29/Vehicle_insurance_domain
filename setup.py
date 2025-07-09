910c9131c3c56740ce637e45dacedc329dffd4fc
“Vivek <viveik29@gmail.com>
2025-07-09 08:47:02 +0530

    Project structure ready env set

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="src",
+    version="0.0.1",
+    author="Vivek",
+    author_email="viveik29@gmail.com",
+    packages=find_packages()
+)
